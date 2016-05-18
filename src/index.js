5f2b6883ca3775f0b96b0461da31304ab263ff08
elsehow <yes@cosmopol.is>
2016-05-17 23:41:31 -0700

    separated out boilerplate from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 const yo = require('yo-yo')
-const {ipcRenderer} = require('electron');
-let el = null
 
-function view (items) {
+function app (emitF) {
 
-  // events bubble up
-  function onclick () {
-    ipcRenderer.send('add-random-number')
-  }
+  function view (state) {
 
-  return yo`<div>
-    Random Numbers
-    <ul>
-      ${items.map(function (item) {
+    // events bubble up
+    function onclick () {
+      emitF('add-random-number')
+    }
+
+    return yo`<div>
+      Random Numbers
+      <ul>
+      ${state.map(function (item) {
         return yo`<li>${item}</li>`
       })}
     </ul>
     <button onclick=${onclick}>Add Random Number</button>
-  </div>`
+    </div>`
+  }
+
+  return view
 }
 
-function update (_, s) {
-  if (el) 
-    return yo.update(el, view(s))
-  el = view(s)
-  return document.body.appendChild(el)
-}
-
-ipcRenderer.on('state', update)
-ipcRenderer.send('get-state')
+module.exports = app
